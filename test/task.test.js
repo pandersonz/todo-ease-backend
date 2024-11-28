5d8f9040f95acedd7702d171cf30427fe1ce0332
Paul L. Anderson <34735613+pandersonz@users.noreply.github.com>
2024-11-28 13:43:02 -0400

    added enpoint for create task

--- test/task.test.js
+++ test/task.test.js
@@ -1,7 +1,9 @@
 const TaskData = require("../task/task.data.js");
-const { getProjectTasks } = require("../task/task.controller.js");
+const { getProjectTasks, createTask } = require("../task/task.controller.js");
 jest.mock("../task/task.data.js", () => ({
   getProjectTasks: jest.fn(),
+  createTask: jest.fn(),
+  getCountProjectTasks: jest.fn(),
 }));
 
 describe("Tasks controller test", () => {
@@ -28,7 +30,7 @@ describe("Tasks controller test", () => {
       email: "john.doe@example.com",
     };
 
-    const req = mockRequest({ user: decodedToken, query: { projectId: 1 } });
+    const req = mockRequest({ user: decodedToken, params: { projectId: 1 } });
     const res = mockResponse();
     await getProjectTasks(req, res);
 
@@ -44,5 +46,32 @@ describe("Tasks controller test", () => {
         },
       ],
     });
+  });
+
+  TaskData.createTask.mockResolvedValue({ insertId: 1 });
+  TaskData.getCountProjectTasks.mockResolvedValue({
+    taskCount: 1,
+    codeProject: "Test",
+  });
+
+  it("it should create a task", async () => {
+    const newTask = {
+      name: "New Task",
+      description: "New Task Description",
+      projectId: 1,
+    };
+
+    const decodedToken = {
+      id: 1,
+      firstname: "John",
+      lastname: "Doe",
+      email: "john.doe@example.com",
+    };
+
+    const req = mockRequest({ body: newTask, user: decodedToken });
+    const res = mockResponse();
+    await createTask(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
   });
 });
